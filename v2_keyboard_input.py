f817612fa00b8fb8c9d280d6e77781e488d87307
Chanakya555 <ccheedi@gitam.in>
2025-08-19 21:33:03 +0530

    Version 2 committed

---
+++ v2_keyboard_input.py
@@ -0,0 +1,7 @@
+a=float(input("Enter coefficient a: "))
+b=float(input("Enter coefficient b: "))
+c=float(input("Enter coefficient c: "))
+t=float(input("Enter time t(hour/day): "))
+
+t=a*t**2+b*t+c
+print(f"Prdicted temperature at t={t}: {t:.2f} degrees Celsius")
