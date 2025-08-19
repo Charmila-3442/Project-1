49b8eecc7399d0273877a3cbf2f905467a40e886
Chanakya555 <ccheedi@gitam.in>
2025-08-19 21:37:04 +0530

    Version 3 committed

---
+++ v3_file_input_single.py
@@ -0,0 +1,10 @@
+with open("inputs_single.txt","r") as f:
+    lines=f.readlines()
+
+a=float(lines[0])
+b=float(lines[1])
+c=float(lines[2])
+t=float(lines[3])
+
+t=a*t**2+b*t+c
+print(f"Prdicted temperature at t={t}: {t:.2f} degrees Celsius")
