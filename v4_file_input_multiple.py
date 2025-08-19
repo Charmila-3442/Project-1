71e4f3a9be0f12153c0d1c1d9a22ed9ad5e1ebe3
Mantri Charmila <cmantri@gitam.in>
2025-08-19 22:27:49 +0530

    Update v4_file_input_multiple.py

--- v4_file_input_multiple.py
+++ v4_file_input_multiple.py
@@ -2,4 +2,4 @@ with open("inputs_multiple.txt","r") as f:
     for lines in f:
         a,b,c,t=map(float,lines.split())
         t=a*t**2+b*t+c
-        print(f"Prdicted temperature at t={t}: {t:.2f} degrees Celsius")
+        print(f"Prdicted temperature at t={t} : {t:.2f} degrees Celsius")
